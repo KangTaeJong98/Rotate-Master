815947e5db12a822d67d2ff772961e5607989962
KangTaeJong98 <rkdxowhd98@naver.com>
2022-02-25 18:36:00 +0900

    [#1][MainScreen] Navigation 적용

---
+++ app/src/main/java/com/taetae98/rotatemaster/ui/navigation/MainNavigation.kt
@@ -0,0 +1,14 @@
+package com.taetae98.rotatemaster.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Preview
+@Composable
+fun MainNavigation(
+    navController: NavHostController = rememberNavController()
+) {
+
+}
