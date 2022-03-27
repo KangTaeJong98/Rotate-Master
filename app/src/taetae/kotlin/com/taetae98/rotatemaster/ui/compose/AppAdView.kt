2ca7856a52a3d192cb1621e13400e43e8391c91e
rkdxowhd98 <rkdxowhd98@webtoonscorp.com>
2022-03-27 17:04:14 +0900

    [#19][RotateNotification] debug release taetae 광고 분리

---
+++ app/src/taetae/kotlin/com/taetae98/rotatemaster/ui/compose/AppAdView.kt
@@ -0,0 +1,11 @@
+package com.taetae98.rotatemaster.ui.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppAdView(
+    modifier: Modifier = Modifier
+) {
+
+}
